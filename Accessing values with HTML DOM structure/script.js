69a3c6092921d78402e40321e270e19375b71145
Maimuna <knbari88@gmail.com>
2024-11-12 18:34:47 +0600

    mhm

--- Accessing values with HTML DOM structure/script.js
+++ Accessing values with HTML DOM structure/script.js
@@ -1,5 +1,5 @@
 function myFunction() {
-  var a = 33;
+  var a = 145;
   document.getElementById("result").innerHTML = a * a;
   document.getElementById("demo").firstChild.nodeValue;
   document.getElementById("result1").innerHTML =
